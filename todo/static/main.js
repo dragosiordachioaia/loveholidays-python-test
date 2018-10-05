60995b9811e11386a9a86c2589e4fbbef60d3892
dragosiordachioaia <xdragosy@gmail.com>
2018-10-05 13:59:15 +0100

    Improve the design a bit, make names not unique in the db

--- todo/static/main.js
+++ todo/static/main.js
@@ -1,6 +1,6 @@
-var API_URL = 'http://127.0.0.1:8000';
+var API_URL = "http://127.0.0.1:8000";
 
-var container = $('#todo-container');
+var container = $("#todo-container");
 var todoListData = [];
 
 refreshListFromServer();
@@ -8,10 +8,10 @@ refreshListFromServer();
 function fetchAllTodos() {
   return axios({
     method: "GET",
-    url: API_URL + '/todos/',
+    url: API_URL + "/todos/",
     headers: {
-       'Content-Type': 'application/json'
-    }
+      "Content-Type": "application/json",
+    },
   });
 }
 
@@ -22,54 +22,57 @@ function refreshListFromServer() {
   });
 }
 
-
-
 $(window).click(unHighlightAllTodos);
-$('button#add').click(addTodo);
+$("button#add").click(addTodo);
 container.click(function(e) {
   e.stopPropagation(); // to prevent this from triggering the global click listener
 });
 
-
 function renderTodos() {
-  container.html('');
-  todoListData.forEach(function(todoData, index) {
-    var newTodo = $('<li class="todo"></li>');
-    var checkbox = $('<input class="checkbox" type="checkbox"/>');
-    var deleteButton = $('<button class="delete"><i class="fa fa-trash"></i></button>')
-    if(todoData.checked) {
-      checkbox.attr('checked', true);
-    }
-    var input;
-    newTodo.append(checkbox);
-    if(todoData.highlighted) {
-      input = $('<input class="todo-name" value="'+todoData.name+'"/>' );
-      input.on('keydown', onKeyDown);
-      newTodo.append(input);
+  container.html("");
+  if (todoListData && todoListData.length > 0) {
+    todoListData.forEach(function(todoData, index) {
+      var newTodo = $('<li class="todo"></li>');
+      var checkbox = $('<input class="checkbox" type="checkbox"/>');
+      var deleteButton = $(
+        '<button class="delete"><i class="fa fa-trash"></i></button>'
+      );
+      if (todoData.checked) {
+        checkbox.attr("checked", true);
+      }
+      var input;
+      newTodo.append(checkbox);
+      if (todoData.highlighted) {
+        input = $('<input class="todo-name" value="' + todoData.name + '"/>');
+        input.on("keydown", onKeyDown);
+        newTodo.append(input);
+      } else {
+        var label = $("<label>" + todoData.name + "</value>");
+        newTodo.append(label);
+        label.click(function() {
+          highlightTodo(index);
+        });
+      }
 
-    } else {
-      var label = $('<label>'+todoData.name+'</value>')
-      newTodo.append(label);
-      label.click(function() {
-        highlightTodo(index);
+      newTodo.append(deleteButton);
+      deleteButton.click(function() {
+        deleteTodo(index);
       });
-    }
+      checkbox.click(function() {
+        checkTodo(index);
+      });
 
-
-    newTodo.append(deleteButton);
-    deleteButton.click(function() {
-      deleteTodo(index);
+      container.append(newTodo);
+      if (input) {
+        input.focus();
+      }
     });
-    checkbox.click(function() {
-      checkTodo(index);
-    });
-
-
-    container.append(newTodo);
-    if(input) {
-      input.focus();
-    }
-  });
+  } else {
+    let noTodosMessage = $(
+      '<p id="no-todos">There are no todos one. <br/>Get started by adding one now!</p>'
+    );
+    container.append(noTodosMessage);
+  }
 }
 
 function checkTodo(index) {
@@ -86,20 +89,23 @@ function deleteTodo(index) {
 }
 
 function highlightTodo(index) {
-  if(todoListData.highlighted) {
+  if (todoListData.highlighted) {
     return;
   }
   todoListData[index].highlighted = true;
   renderTodos();
 }
 
-
 function unHighlightAllTodos() {
-  console.log('here');
+  console.log("here");
   todoListData.forEach(function(todoData, index) {
-    if(todoData.highlighted) {
+    if (todoData.highlighted) {
       apiUpdateTodo(index);
-      var value = $('.todo').eq(index).find('.todo-name').first().val();
+      var value = $(".todo")
+        .eq(index)
+        .find(".todo-name")
+        .first()
+        .val();
       todoListData[index].name = value;
     }
     todoData.highlighted = false;
@@ -111,19 +117,18 @@ function addTodo() {
   apiCreateTodo(refreshListFromServer);
 }
 
-
 function onServerError() {
   refreshListFromServer();
-  alert('Autch! The last operation didn\'t work! Please try again.');
+  alert("Autch! The last operation didn't work! Please try again.");
 }
 
 function apiUpdateTodo(index) {
   var id = todoListData[index].id;
   axios({
     method: "PUT",
-    url: API_URL + '/todos/' + id + '/',
+    url: API_URL + "/todos/" + id + "/",
     headers: {
-       'Content-Type': 'application/json'
+      "Content-Type": "application/json",
     },
     data: todoListData[index],
   }).then(function() {}, onServerError);
@@ -133,28 +138,28 @@ function apiDeleteTodo(index) {
   var id = todoListData[index].id;
   axios({
     method: "DELETE",
-    url: API_URL + '/todos/' + id + '/',
+    url: API_URL + "/todos/" + id + "/",
     headers: {
-       'Content-Type': 'application/json'
+      "Content-Type": "application/json",
     },
   }).then(function() {}, onServerError);
 }
 function apiCreateTodo(callback) {
   axios({
     method: "POST",
-    url: API_URL + '/todos/',
+    url: API_URL + "/todos/",
     headers: {
-       'Content-Type': 'application/json'
+      "Content-Type": "application/json",
     },
     data: {
       checked: false,
       name: "New Todo",
-    }
+    },
   }).then(callback, onServerError);
 }
 
 function onKeyDown(e) {
-  if(e.keyCode === 13) {
+  if (e.keyCode === 13) {
     unHighlightAllTodos();
   }
 }
