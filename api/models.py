e83c72f31e32d5143e1bb6fd36652e8f3f70c8c7
dragosiordachioaia <xdragosy@gmail.com>
2018-10-05 14:14:53 +0100

    Make name unique again, ev

--- api/models.py
+++ api/models.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 from django.db import models
 
 class Todo(models.Model):
-    name = models.CharField(max_length=255, blank=False, unique=False)
+    name = models.CharField(max_length=255, blank=False, unique=True)
     checked = models.BooleanField(blank=False, unique=False, default=False)
     date_created = models.DateTimeField(auto_now_add=True)
     date_modified = models.DateTimeField(auto_now=True)
